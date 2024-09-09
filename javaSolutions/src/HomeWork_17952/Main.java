5b6c121e6daac76b4bc16db8329196ac6a462e63
hdXian <jason123325@gmail.com>
2024-09-09 23:19:19 +0900

    #3 just refactoring code

--- javaSolutions/src/HomeWork_17952/Main.java
+++ javaSolutions/src/HomeWork_17952/Main.java
@@ -19,32 +19,35 @@ public class Main {
         Work currentWork = null;
         int totalScore = 0;
 
+        int workTime = 0;
+
+        // 시간동안 진행
         for (int i = 0; i < n; i++) {
             String[] line = reader.readLine().split(" "); // 1(or 0) A T
             boolean isWorkAdd = line[0].equals("1");
 
+            // 하는 일도 없고 추가된 일도 없을 때 건너뜀
+            if(currentWork == null && !isWorkAdd)
+                continue;
+
             // 일이 추가되면 현재 일을 변경
             if (isWorkAdd) {
-                // 하던 일은 큐에 넣어둠 (가장 뒤로, 참조 전달)
-                if (currentWork != null)
-                    works.add(0, currentWork);
+                // 하던 일은 큐에 넣어둠 (가장 앞으로, 참조 전달)
+//                if (currentWork != null)
+                works.add(0, currentWork);
 
                 // 현재 일을 새로 들어온 일로 변경
                 score = Integer.parseInt(line[1]);
                 t = Integer.parseInt(line[2]);
                 System.out.printf("Work added: score=%d, time=%d\n", score, t);
-                currentWork = new Work(score, t);
-                currentWork.remainTime--; // 받자마자 일을 함
+                currentWork = new Work(score, t-1); // 받자마자 일을 함
             }
-            else { // 일이 추가되지 않으면
-                // 하는 일이 있다면 남은시간 감소
-                if(currentWork != null) {
-                    currentWork.remainTime--;
-                }
+            else { // 일이 추가된게 아니면 하던 일을 수행 (남은시간 감소)
+                currentWork.remainTime--;
             }
 
             // 시간이 다 되면 (일을 마치면) 점수를 더하고 다음 일을 꺼냄.
-            if((currentWork != null) && currentWork.remainTime == 0) {
+            if(currentWork.remainTime == 0) {
                 System.out.println("complete work: " + currentWork);
 
                 totalScore += currentWork.score;
