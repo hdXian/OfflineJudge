442d823ed03dabb9f3b599259e48715a6cd6da25
hdXian <jason123325@gmail.com>
2025-02-20 03:11:35 +0900

    #41 2 - using binary search.. i saw solution. my poor skill.

--- javaSolutions/src/prob_202502/week4/numberbiz_2613/Main.java
+++ javaSolutions/src/prob_202502/week4/numberbiz_2613/Main.java
@@ -18,6 +18,7 @@ public class Main {
         M = Integer.parseInt(tkn.nextToken()); // 그룹 수 (1 ~ N)
 
         balls = new int[N];
+        int max_ball = 0;
 
         // 꿰어진 구슬 입력받기
         int tmp;
@@ -27,9 +28,11 @@ public class Main {
             tmp = Integer.parseInt(tkn.nextToken());
             balls[i] = tmp;
             total += tmp;
+            if (tmp > max_ball)
+                max_ball = tmp;
         }
 
-        binarySearch(0, total);
+        binarySearch(max_ball, total);
     }
 
     // 첫 요소부터 차례대로 붙여서 기준점을 넘지 않을 때까지 이어붙이기
@@ -41,7 +44,7 @@ public class Main {
 
         // 기준점 -> 각 그룹의 합의 최댓값의 기준
         int mid;
-        List<Set<Integer>> groups = new ArrayList<>();
+        List<List<Integer>> groups = new ArrayList<>();
 
         while (left <= right) {
             mid = (left + right) / 2; // 기준점
@@ -54,11 +57,7 @@ public class Main {
                 left = mid+1;
             }
             // 만들어진 그룹이 M보다 적다 -> 기준값을 줄여야 한다.
-            else if (size < M) {
-                right = mid-1;
-            }
-            // 만들어진 그룹이 M과 같다 -> 기준값을 줄여도 조건을 만족할 수도 있다.
-            else {
+            else if (size <= M) {
                 result = mid;
                 right = mid-1;
             }
@@ -70,7 +69,7 @@ public class Main {
         StringBuilder sb = new StringBuilder();
         sb.append(result).append("\n");
 
-        for (Set<Integer> e: groups) {
+        for (List<Integer> e: groups) {
             sb.append(e.size()).append(" ");
         }
 
@@ -81,13 +80,13 @@ public class Main {
 
     // 그룹 만들기 (val 값을 기준으로)
     // 이 함수는 그룹을 짓고, 조건을 만족하면
-    public static List<Set<Integer>> makeGroup(int val) {
+    public static List<List<Integer>> makeGroup(int val) {
         System.out.println("val = " + val);
 
-        List<Set<Integer>> groups = new ArrayList<>();
+        List<List<Integer>> groups = new ArrayList<>();
 
         // 모든 구슬을 돌며 그룹을 만든다.
-        Set<Integer> group = new HashSet<>();
+        List<Integer> group = new ArrayList<>();
         int group_sum = 0;
         for (int i=0; i<N; i++) {
             System.out.println("i = " + i);
@@ -99,7 +98,7 @@ public class Main {
             }
             else {
                 groups.add(group);
-                group = new HashSet<>();
+                group = new ArrayList<>();
 
                 group_sum = balls[i];
                 group.add(balls[i]);
@@ -111,7 +110,7 @@ public class Main {
             groups.add(group);
         }
 
-        for(Set<Integer> s: groups) {
+        for(List<Integer> s: groups) {
             System.out.println(s);
         }
 
